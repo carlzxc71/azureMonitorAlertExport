495bfbfc9f49aaf20207a7fb6a6f45793515d00e
Carl Lindberg <carl.lindberg@iver.se>
2023-03-22 14:58:43 +0100

    added script to list alerts

---
+++ src/List-PastAlerts.ps1
@@ -0,0 +1,13 @@
+## Set correct subscription
+
+Select-AzSubscription -Subscription "b55e1bab-2d62-42f4-8d58-feeb80f33f7e"
+
+## List past alerts
+
+# Define the start and end time for the custom time range
+$startTime = (Get-Date).AddDays(-25)
+$endTime = Get-Date -Format yyyy-MM-dd
+
+# Retrieve the alerts for the custom time range
+Get-AzAlert -CustomTimeRange "$(Get-Date $startTime -Format yyyy-MM-dd)/$endTime"
+
