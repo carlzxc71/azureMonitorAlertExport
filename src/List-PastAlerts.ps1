9d546b5a4fc83d5e4b6d4c873e7d4acc0adf03a8
Carl Lindberg <carl.lindberg@iver.se>
2023-04-06 09:52:28 +0200

    removed old script and modified powershell script to output to storage

--- src/List-PastAlerts.ps1
+++ src/List-PastAlerts.ps1
@@ -26,4 +26,19 @@ $endTime = Get-Date -Format yyyy-MM-dd
 $Alerts = Get-AzAlert -CustomTimeRange "$(Get-Date $startTime -Format yyyy-MM-dd)/$endTime" | Select-Object -Property Name,StartDateTime,TargetResource,MonitorCondition,MonitorConditionResolvedDateTime | 
 Sort-Object StartDateTime 
 
-return $Alerts
+# Export data to CSV
+$Alerts| Export-Csv -Path ".\alerts.csv" -NoTypeInformation
+
+# Create a storage context using the system-assigned managed identity
+$StorageAccountName = "stgazuremonitoralert001"
+$StorageAccountResourceGroup = "rg-monitor-automation-001"
+$StorageContext = New-AzStorageContext -StorageAccountName $StorageAccountName `
+                                       -ResourceGroupName $StorageAccountResourceGroup
+                                       -UseConnectedAccount `
+
+# Upload the CSV file to Storage Account
+$ShareName = "share01"
+$DirectoryName = "directory01"
+$FileName = "alerts.csv"
+$FilePath = ".\alerts.csv"
+Set-AzStorageFileContent -ShareName $ShareName -Context $StorageContext -Source $FilePath -Path $DirectoryName/$FileName -Force
