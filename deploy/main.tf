2682267fb17db4a31d522ced24ef64f841c075b1
Carl Lindberg <carl.lindberg@iver.se>
2023-04-18 08:52:16 +0200

    renamed symbolic names for reosurces to adhere to naming conventions

--- deploy/main.tf
+++ deploy/main.tf
@@ -1,18 +1,21 @@
-// Resource group configuration
+data "local_file" "script" {
+  filename = "../src/List-PastAlerts.ps1"
+}
 
-resource "azurerm_resource_group" "rg-monitor-automation" {
+data "azurerm_subscription" "primary" {
+}
+
+resource "azurerm_resource_group" "rg" {
   name     = var.rg_name
   location = var.rg_location
 
   tags = local.tags
 }
 
-## Automation account configuration
-
-resource "azurerm_automation_account" "aa-monitor-automation" {
+resource "azurerm_automation_account" "account" {
   name                = var.aa_account_name
-  location            = azurerm_resource_group.rg-monitor-automation.location
-  resource_group_name = azurerm_resource_group.rg-monitor-automation.name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
   sku_name            = var.aa_account_sku
 
   identity {
@@ -22,19 +25,12 @@ resource "azurerm_automation_account" "aa-monitor-automation" {
   tags = local.tags
 }
 
-data "local_file" "script" {
-  filename = "../src/List-PastAlerts.ps1"
-}
 
-data "azurerm_subscription" "primary" {
-}
-
-
-resource "azurerm_automation_runbook" "aa-runbook" {
+resource "azurerm_automation_runbook" "runbook" {
   name                    = "Get-AzureMonitorAlerts"
-  location                = azurerm_resource_group.rg-monitor-automation.location
-  resource_group_name     = azurerm_resource_group.rg-monitor-automation.name
-  automation_account_name = azurerm_automation_account.aa-monitor-automation.name
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  automation_account_name = azurerm_automation_account.account.name
   log_verbose             = "true"
   log_progress            = "true"
   description             = "Run this script to get past Azure Monitor Alerts"
@@ -47,8 +43,8 @@ resource "azurerm_automation_runbook" "aa-runbook" {
 
 resource "azurerm_automation_schedule" "schedule" {
   name                    = "powershell-automation-schedule"
-  resource_group_name     = azurerm_resource_group.rg-monitor-automation.name
-  automation_account_name = azurerm_automation_account.aa-monitor-automation.name
+  resource_group_name     = azurerm_resource_group.rg.name
+  automation_account_name = azurerm_automation_account.account.name
   frequency               = "Month"
 
   monthly_occurrence {
@@ -61,61 +57,59 @@ resource "azurerm_automation_schedule" "schedule" {
 
 resource "azurerm_automation_job_schedule" "jobschedule" {
   schedule_name           = azurerm_automation_schedule.schedule.name
-  resource_group_name     = azurerm_resource_group.rg-monitor-automation.name
-  automation_account_name = azurerm_automation_account.aa-monitor-automation.name
-  runbook_name            = azurerm_automation_runbook.aa-runbook.name
+  resource_group_name     = azurerm_resource_group.rg.name
+  automation_account_name = azurerm_automation_account.account.name
+  runbook_name            = azurerm_automation_runbook.runbook.name
 }
 
-resource "azurerm_automation_variable_string" "secret-variable" {
+resource "azurerm_automation_variable_string" "secret" {
   name                    = "accountKey"
-  resource_group_name     = azurerm_resource_group.rg-monitor-automation.name
-  automation_account_name = azurerm_automation_account.aa-monitor-automation.name
-  value                   = azurerm_storage_account.stg-monitor.primary_access_key
+  resource_group_name     = azurerm_resource_group.rg.name
+  automation_account_name = azurerm_automation_account.account.name
+  value                   = azurerm_storage_account.stg.primary_access_key
   encrypted               = true
   description             = "Account key for access to Azure Storage Account"
 }
 
-resource "azurerm_automation_variable_string" "storageacc-variable" {
+resource "azurerm_automation_variable_string" "storage_name_var" {
   name                    = "storageAccountName"
-  resource_group_name     = azurerm_resource_group.rg-monitor-automation.name
-  automation_account_name = azurerm_automation_account.aa-monitor-automation.name
-  value                   = azurerm_storage_account.stg-monitor.name
+  resource_group_name     = azurerm_resource_group.rg.name
+  automation_account_name = azurerm_automation_account.account.name
+  value                   = azurerm_storage_account.stg.name
   encrypted               = false
   description             = "The name of the Storage Account"
 }
 
 resource "azurerm_role_assignment" "contributor" {
-  scope                = azurerm_resource_group.rg-monitor-automation.id
+  scope                = azurerm_resource_group.rg.id
   role_definition_name = "Contributor"
-  principal_id         = azurerm_automation_account.aa-monitor-automation.identity[0].principal_id
+  principal_id         = azurerm_automation_account.account.identity[0].principal_id
 }
 
-resource "azurerm_role_assignment" "monitor-reader" {
+resource "azurerm_role_assignment" "monitor_reader" {
   scope                = data.azurerm_subscription.primary.id
   role_definition_name = "Monitoring Reader"
-  principal_id         = azurerm_automation_account.aa-monitor-automation.identity[0].principal_id
+  principal_id         = azurerm_automation_account.account.identity[0].principal_id
 }
 
-## Storage account configuration
-
-resource "azurerm_storage_account" "stg-monitor" {
+resource "azurerm_storage_account" "stg" {
   name                     = var.stg_name
-  resource_group_name      = azurerm_resource_group.rg-monitor-automation.name
-  location                 = azurerm_resource_group.rg-monitor-automation.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
   tags = local.tags
 }
 
-resource "azurerm_storage_share" "stg-share" {
+resource "azurerm_storage_share" "share" {
   name                 = var.stg_share_name
-  storage_account_name = azurerm_storage_account.stg-monitor.name
+  storage_account_name = azurerm_storage_account.stg.name
   quota                = var.stg_share_quota
 }
 
-resource "azurerm_storage_share_directory" "stg-dir" {
+resource "azurerm_storage_share_directory" "directory" {
   name                 = var.stg_directory_name
-  share_name           = azurerm_storage_share.stg-share.name
-  storage_account_name = azurerm_storage_account.stg-monitor.name
+  share_name           = azurerm_storage_share.share.name
+  storage_account_name = azurerm_storage_account.stg.name
 }
