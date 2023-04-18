faeed2c03610b496f8fe7ce4d21971a7510dd283
Carl Lindberg <carl.lindberg@iver.se>
2023-04-18 09:42:17 +0200

    locked in tls version

--- deploy/main.tf
+++ deploy/main.tf
@@ -98,6 +98,7 @@ resource "azurerm_storage_account" "stg" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
 
   tags = local.tags
 }
